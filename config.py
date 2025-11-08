a952384b1f395a82b879aadf74c15253d7dead73
Rupali Shewale <rupalishewale2005@gmail.com>
2025-11-08 21:11:05 +0530

    Created

---
+++ config.py
@@ -0,0 +1,11 @@
+
+
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "Rupali09"
+MYSQL_HOST = "localhost"
+MYSQL_DB = "medical_lab_db"
+
+SQLALCHEMY_DATABASE_URI = (
+    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
